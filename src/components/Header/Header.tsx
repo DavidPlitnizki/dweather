0107a00d5894fa0cd1a3d571e7f2bd046262a7f9
DavidPlitnizki <dplitnizki@gmail.com>
2024-12-11 22:41:02 +0200

    rewrite vite apps

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styles from './Header.module.scss';
+import React from "react";
+import styles from "./Header.module.css";
 
-const Header:React.FC<{}> = () => {
-    return (
-        <header className={styles.wrapper}>
-            <h1>D - WEATHER</h1>
-        </header>
-    )
-}
+const Header: React.FC<{}> = () => {
+  return (
+    <header className={styles.wrapper}>
+      <h1>D - WEATHER</h1>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,57 +1,65 @@
-import React,{useState, useEffect, useCallback} from 'react';
-import styles from './Search.module.scss'; 
-import { FormControl, InputLabel, Input, FormHelperText  } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState, useEffect, useCallback } from "react";
+import styles from "./Search.module.css";
+import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 interface Props {
-    searchByCityName: (cityName: string)=> void
+  searchByCityName: (cityName: string) => void;
 }
 
-const Search: React.FC<Props> = ({searchByCityName}) => {
-    const [search, setSearch] = useState<string>("");
-    
+const Search: React.FC<Props> = ({ searchByCityName }) => {
+  const [search, setSearch] = useState<string>("");
 
-    const handleSubmit= useCallback(() => {
-            searchByCityName(search);
-            clearField();
-    },[search, searchByCityName]);
+  const handleSubmit = useCallback(() => {
+    searchByCityName(search);
+    clearField();
+  }, [search, searchByCityName]);
 
-    function onCandleChange(e: React.ChangeEvent<HTMLInputElement>) {
-        setSearch(e.target.value);
-    }
+  function onCandleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearch(e.target.value);
+  }
 
-    const filterOnKeyPress = useCallback((e: KeyboardEvent) => {
-        if (e.key === "Enter") {
-            handleSubmit(); 
-        }
-    },[handleSubmit]);
+  const filterOnKeyPress = useCallback(
+    (e: KeyboardEvent) => {
+      if (e.key === "Enter") {
+        handleSubmit();
+      }
+    },
+    [handleSubmit]
+  );
 
-    const clearField = () => {
-        setSearch("");
-    }
+  const clearField = () => {
+    setSearch("");
+  };
 
-    useEffect(()=> {
-    window.addEventListener('keypress', filterOnKeyPress, true);
-    return (()=> {
-        window.removeEventListener('keypress', filterOnKeyPress, true);
-    })
-    },[filterOnKeyPress])
-    
-    return (
-        <section className={styles.wrapper_search}>
-            <FormControl className={styles.form}>
-            <div>
-                <InputLabel htmlFor="search">City</InputLabel>
-                <Input id="search" aria-describedby="helper-text" value={search} onChange={onCandleChange} />
-                <FormHelperText id="helper-text">Enter your intersting place.</FormHelperText>
-            </div>
-            <div>
-                <SearchIcon className={styles.icon} onClick={handleSubmit} />
-            </div>
-                
-            </FormControl>
-        </section>
-    )
-}
+  useEffect(() => {
+    window.addEventListener("keypress", filterOnKeyPress, true);
+    return () => {
+      window.removeEventListener("keypress", filterOnKeyPress, true);
+    };
+  }, [filterOnKeyPress]);
+
+  return (
+    <section className={styles.wrapper_search}>
+      <FormControl className={styles.form}>
+        <div>
+          <InputLabel htmlFor="search">City</InputLabel>
+          <Input
+            id="search"
+            aria-describedby="helper-text"
+            value={search}
+            onChange={onCandleChange}
+          />
+          <FormHelperText id="helper-text">
+            Enter your intersting place.
+          </FormHelperText>
+        </div>
+        <div>
+          <SearchIcon className={styles.icon} onClick={handleSubmit} />
+        </div>
+      </FormControl>
+    </section>
+  );
+};
 
 export default Search;
